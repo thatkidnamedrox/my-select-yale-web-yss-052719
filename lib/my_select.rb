f499cc0a8a3d1f9571af3f76f26fed762425a923
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 12:11:31 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
  # your code here!
+ if block_given?
+   i = 0
+   result = []
+   while i < array.length
+     if yield(array[i]) == true
+       result << array[i]
+     end
+   end
+   result
+ else
+   nil
+ end
 end
