0a8f35c9cef3ae9b18d5463e1db1bfd1783e4706
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-05-22 12:15:03 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
 def my_select(collection)
  # your code here!
+ if block_given?
+
+   i = 0
+   result = []
+   while i < collection.length
+     if yield(collection[i]) == true
+       result << collection[i]
+     end
+     i+=1
+   end
+   result
+ else
+   nil
+ end
 end
